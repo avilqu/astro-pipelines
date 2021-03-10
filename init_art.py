c2d5789917f9b9cb1d212daddb74367bbfa76ba9
Adrien Vilquin Barrajon <avilqu@gmail.com>
2021-03-11 04:47:01 +1300

    added folder init scripts

---
+++ init_art.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+from pathlib import Path
+
+raw_path_str = os.getcwd()
+new_path_str = raw_path_str.replace('data', 'photography', 1)
+
+new_path = Path(new_path_str)
+new_path.mkdir(parents=True, exist_ok=True)
+os.symlink(raw_path_str, new_path_str + '/raw')
+os.symlink(new_path_str, raw_path_str + '/reduced')
+
+os.chdir(new_path_str)
