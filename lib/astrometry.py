1318d89b8c6ccbad0d0170c238c5aab03d62a127
Adri Vilba <avilqu@gmail.com>
2022-10-27 04:36:22 +1300

    sso_overlay now functional

--- lib/astrometry.py
+++ lib/astrometry.py
@@ -20,81 +20,23 @@ def query_skybot(img):
 def overlay_sso(img):
     d = pyds9.DS9()
     filename = img['filename']
+    data = CCDData.read(filename)
     d.set(f'file new {filename}')
     d.set('zoom to fit')
     d.set('scale zscale')
 
     objects = []
-    try:
-        for obj in query_skybot(img):
-            objects.append({
-                'name': obj['Name'],
-                'mag': obj['V'],
-                'type': obj['Type'],
-                'geodist': obj['geodist'],
-                'coord': SkyCoord(obj['RA'], obj['DEC'])
-            })
-        for obj in objects:
-            # coord = img.wcs.world_to_pixel(obj['coord'])
-            d.set('regions', f'circle({coord[0]},{coord[1]},15)')
+    for obj in query_skybot(img):
+        objects.append({
+            'name': obj['Name'],
+            'mag': obj['V'],
+            'type': obj['Type'],
+            'geodist': obj['geodist'],
+            'coord': SkyCoord(obj['RA'], obj['DEC'])
+        })
+    for obj in objects:
+        coord = data.wcs.world_to_pixel(obj['coord'])
+        d.set('regions', f'circle({coord[0]},{coord[1]},15)')
 
-        print(objects)
-        d.set('regions', f'fk5; circle({obj["coord"]},15)')
-    except:
-        print('No solar system object found.')
-
-
-
-
-
-# if __name__ == "__main__":
-
-#     import argparse
-
-#     parser = argparse.ArgumentParser(
-#         description='Image analysis tool. Use with ds9.')
-#     parser.add_argument(
-#         'file', help='input files (FITS only)', type=str)
-#     parser.add_argument('-a', '--all',
-#                         action='store_true', help='runs all the below options')
-#     parser.add_argument('-H', '--header',
-#                         action='store_true', help='print a summary of the FITS header')
-#     parser.add_argument('-W', '--wcs',
-#                         action='store_true', help='finds and prints WCS data')
-#     parser.add_argument('-A', '--astrometry',
-#                         action='store_true', help='annotate solar system objects')
-#     args = parser.parse_args()
-
-#     img = CCDData.read(args.file)
-
-#     d = pyds9.DS9()
-#     d.set(f'file new {args.file}')
-#     d.set('zoom to fit')
-#     d.set('scale zscale')
-
-#     if args.header or args.all:
-#         print_header_summary(img, ['object', 'date-obs', 'frame',
-#                              'instrume', 'filter', 'exptime', 'ccd-temp', 'gain', 'offset', 'naxis1', 'naxis2'])
-
-#     if args.wcs or args.all:
-#         print_wcs(img)
-
-#     if args.astrometry or args.all:
-#         objects = []
-#         try:
-#             for obj in query_skybot(img):
-#                 objects.append({
-#                     'name': obj['Name'],
-#                     'mag': obj['V'],
-#                     'type': obj['Type'],
-#                     'geodist': obj['geodist'],
-#                     'coord': SkyCoord(obj['RA'], obj['DEC'])
-#                 })
-#             for obj in objects:
-#                 coord = img.wcs.world_to_pixel(obj['coord'])
-#                 d.set('regions', f'circle({coord[0]},{coord[1]},15)')
-
-#             print(objects)
-#             d.set('regions', f'fk5; circle({obj["coord"]},15)')
-#         except:
-#             print('No solar system object found.')
+    print(objects)
+    d.set('regions', f'fk5; circle({obj["coord"]},15)')
